edaf461c8f128e3a3e093bdb0c26bc5c93fe45c0
Nasir Lawal <nasirlawal001@gmail.com>
2024-07-02 09:08:39 +0100

    setup project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="tw-bg-white tw-h-16">
+      <q-toolbar class="tw-mt-2">
+        <q-img
+          src="/assets/svg/logo.svg"
+          class="tw-w-16"
+        />
+        <q-space />
+        <q-btn
+          flat
+          dense
+          round
+          color="black"
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+    >
+
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <div class="tw-text-4xl tw-text-green-600 t">
+      asdasd
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
